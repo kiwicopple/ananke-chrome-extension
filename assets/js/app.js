fc4e424b0702c3a68d7c6c5075133b0a76e9054f
kiwicopple <pcopplestone@gmail.com>
2018-07-19 23:03:59 +0800

    ignoring bower components

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,30 @@
-function getTimeRemaining(endtime) {
+// var upcomingList = JSON.parse(localStorage.getItem("upcominglist"))
+document.addEventListener('DOMContentLoaded', documentEvents, false);
+
+function  documentEvents () {
+  var dateInputButton = document.getElementById('dateInputButton')
+  dateInputButton.addEventListener('click', handleFormSubmit)
+}
+
+
+var handleFormSubmit = function (e) {
+  var upcomingInput = document.getElementById('upcomingInput')
+  var dateInput = document.getElementById('dateInput')
+  var upcoming = upcomingInput.value
+  var rawDateString = dateInput.value
+  try {
+    var upcomingDate = parseDate(rawDateString)[0]
+    console.log('upcomingDate', upcomingDate)
+  } catch (e) {
+    console.log('e', e)
+  }
+}
+
+var parseDate = function (rawDateString) {
+  return chrono.parse('An appointment on Sep 12-13')
+}
+
+var getTimeRemaining = function (endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
@@ -13,30 +39,30 @@ function getTimeRemaining(endtime) {
   };
 }
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+var initializeClock = function (id, endtime) {
+  var clock = document.getElementById(id)
+  var daysSpan = clock.querySelector('.days')
+  var hoursSpan = clock.querySelector('.hours')
+  var minutesSpan = clock.querySelector('.minutes')
+  var secondsSpan = clock.querySelector('.seconds')
 
   function updateClock() {
-    var t = getTimeRemaining(endtime);
+    var t = getTimeRemaining(endtime)
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    daysSpan.innerHTML = t.days
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2)
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2)
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2)
 
     if (t.total <= 0) {
-      clearInterval(timeinterval);
+      clearInterval(timeinterval)
     }
   }
 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
+  updateClock()
+  var timeinterval = setInterval(updateClock, 1000)
 }
 
-var deadline = new Date(Date.parse(new Date('August 9, 2018 20:10:00')));
+var deadline = new Date(Date.parse(new Date('August 9, 2018 20:10:00')))
 
-initializeClock('clockdiv', deadline);
+initializeClock('clockdiv', deadline)
